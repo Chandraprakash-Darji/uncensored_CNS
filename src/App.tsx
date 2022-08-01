8bb85b95f322101ea2e7df92fa1e1592376936e5
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-01 16:09:41 +0530

    eslint fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-function App() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen text-white bg-gradient-to-br from-pink-700 to-blue-800 text-8xl font-bold">
-      Uncensored CNS
-    </div>
-  );
-}
+import React from 'react';
+/**
+ * @return {JSX.Element}
+ */
+const App: React.FC = ()  => (
+  <div className="flex flex-col items-center justify-center h-screen text-white bg-gradient-to-br from-pink-700 to-blue-800 text-8xl font-bold">
+    Uncensored CNS
+  </div>
+);
 
 export default App;
