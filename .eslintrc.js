f5ba88fb66ca2b5ff2242d3748c78e7d0f809279
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-01 16:19:15 +0530

    eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,7 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: [
-    'google',
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended'
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
