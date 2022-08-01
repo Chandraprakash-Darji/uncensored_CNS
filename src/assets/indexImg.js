f4342d78eaa8c70c3c29413e128fede0c58919e8
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-01 11:09:08 +0530

    Starting Again From Zero

--- src/assets/indexImg.js
+++
@@ -1,9 +0,0 @@
-import abstract from "./abstract.svg";
-import polygonSvg from "./polygon.svg";
-
-import MananSoniProfile from './TeamImg/Manan_Soni1.jpg'
-import MananDhariaProfile from './TeamImg/Manan_Dharia1.jpg'
-import DhrumitProfile from './TeamImg/Dhrumit_Patel1.jpg'
-import CPProfile from './TeamImg/Moonlight1.jpg'
-
-export { abstract, polygonSvg, DhrumitProfile, MananDhariaProfile, MananSoniProfile, CPProfile };
