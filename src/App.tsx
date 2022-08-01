3f4fd00bbd5dbb0ae919158c06570828387ef96d
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-01 15:52:51 +0530

    Basic Setup Done

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-white bg-gradient-to-br from-pink-700 to-blue-800 text-8xl font-bold">
+      Uncensored CNS
+    </div>
+  );
+}
+
+export default App;
